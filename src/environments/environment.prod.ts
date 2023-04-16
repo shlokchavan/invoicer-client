0344f23a3ad3c63361b2cd0d2a8e7965696c5e05
Shlok Chavan <shlok@pentaknot.com>
2023-04-16 15:22:46 +0530

    Inital Build Deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "",
+  loginAdmin: {
+    // userName: 'as@unifiedlogs.com',
+    // password: 'Abc@123'
+    userName: 'jess.lang',
+    password: 'Abc@123'
+  },
 };
